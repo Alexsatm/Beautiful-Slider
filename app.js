97e579628bd46c17425ac6077e0f112e8a9bba98
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-11 20:22:57 +0300

    add js listener

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ const container = document.querySelector('.container');
 const slidesCount = mainSlide.querySelectorAll('div').length;
 
 let activeSlideIndex = 0;
+
+sideBar.style.top = `-${(slidesCount -1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+document.addEventListener('keydown', event =>{
+    if(event.key === 'ArronUp'){
+        changeSlide('up')
+    } else if (event.key === 'ArrowDown'){
+        changeSlide('down')
+    }
+})
